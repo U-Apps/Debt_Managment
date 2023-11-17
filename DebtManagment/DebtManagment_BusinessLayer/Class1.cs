2446d9407f5e263eee919e783e58f8197158ca61
Saleh Mohammed <salhbnsmyd3@gmail.com>
2023-11-18 00:38:45 +0300

    Adding the solution that contains the three layers

---
+++ DebtManagment/DebtManagment_BusinessLayer/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DebtManagment_DataAccessLayer; // refernecing the database Layer ,,,,
+namespace DebtManagment_BusinessLayer
+{
+    public class Class1
+    {
+
+        // not implemented yet
+    }
+}
---
+++ DebtManagment/DebtManagment_DataAccessLayer/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtManagment_DataAccessLayer
+{
+    public class datea
+    {
+        // not implemented yet
+    }
+}
