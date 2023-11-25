e51909c01eda96b7f8d573308c4d01d6e3e554ac
faroog25x <faroogbajahaw25@gmail.com>
2023-11-25 15:07:10 +0300

    Add AddNewSupplier() method

--- DebtManagment/DebtManagment_DataAccessLayer/SupplierData.cs
+++ DebtManagment/DebtManagment_DataAccessLayer/SupplierData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,52 @@ namespace DebtManagment_DataAccessLayer
 {
     public class clsSupplierData
     {
-        // Test comment
+        public static int AddNewSupplier(string Name, string Email, string Phone, string Address,
+           int Commercial_Registration)
+        {
+            //this function will return the new supplier id if succeeded and -1 if not.
+            int SupplierID = -1;
+
+            int PersonID = clsPersonData.AddNewPerson(Name, Email, Phone, Address);
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO tblSuppliers
+                            values (@PersonID,@Commercial_Registration)
+                                SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@PersonID", PersonID);
+            command.Parameters.AddWithValue("@Commercial_Registration", Commercial_Registration);
+
+
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    SupplierID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return SupplierID;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
+            return SupplierID;
+        }
     }
 }
