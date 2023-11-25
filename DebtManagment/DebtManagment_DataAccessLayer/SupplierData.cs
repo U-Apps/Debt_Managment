54473131bca8ed4168f5f83f11ec53538e06d76a
faroog25x <faroogbajahaw25@gmail.com>
2023-11-25 16:32:16 +0300

    Update SupplierData class

--- DebtManagment/DebtManagment_DataAccessLayer/SupplierData.cs
+++ DebtManagment/DebtManagment_DataAccessLayer/SupplierData.cs
@@ -56,5 +56,182 @@ namespace DebtManagment_DataAccessLayer
 
             return SupplierID;
         }
+
+        public static bool GetSupplierInfoByID(int SupplierID, ref string Name,
+        ref string Email, ref string Phone, ref string Address,
+           ref int Commercial_Registration)
+        {
+            bool isFound = false;
+
+
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM tblSuppliers WHERE SupplierID = @SupplierID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@SupplierID", SupplierID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // The record was found
+                    isFound = true;
+                    int PersonID = (int)reader["PersonID"];
+
+                    if (!clsPersonData.GetPersonInfoByID(PersonID, ref Name, ref Email, ref Phone, ref Address))
+                        return false;
+
+                    Commercial_Registration = (int)reader["Commercial_Registration"];
+                   
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
+        public static bool UpdateSupplier(int SupplierID, string Name, string Email, string Phone, string Address,
+            int Commercial_Registration)
+        {
+
+            int PersonID = _GetPersonID_BySupplierID(SupplierID);
+            if (PersonID == 0)
+                return false;
+
+
+            clsPersonData.UpdatePerson(PersonID, Name, Email, Phone, Address);
+
+
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Update  tblSuppliers  
+                            set  Commercial_Registration = @Commercial_Registration 
+                                 where SupplierID = @SupplierID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@SupplierID", SupplierID);
+            command.Parameters.AddWithValue("@Commercial_Registration", Commercial_Registration);
+
+            
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+
+        public static bool DeleteSupplier(int SupplierID)
+        {
+            int PersonID = _GetPersonID_BySupplierID(SupplierID);
+            if (PersonID == 0)
+                return false;
+
+
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"Delete tblSuppliers 
+                                where SupplierID =@SupplierID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@SupplierID", SupplierID);
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+                clsPersonData.DeletePerson(PersonID);        //will delete the person record after deleting the supplier record
+
+            }
+            catch (Exception ex)
+            {
+                // Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+
+                connection.Close();
+
+            }
+
+            return (rowsAffected > 0);
+
+        }
+
+        static int _GetPersonID_BySupplierID(int SupplierID)
+        {
+            int PersonID = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT PersonID FROM tblSuppliers WHERE SupplierID = @SupplierID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@SupplierID", SupplierID);
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+
+                if (result != null)
+                {
+                    PersonID = (int)result;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return PersonID;
+        }
     }
 }
