1ad6b7f5f3ec2cd29bbf5f4c37979c95ad14a1f1
Yousef Binnob <yusufbinnob7111@gmail.com>
2023-11-30 14:16:23 +0300

    creating clsDataPaterns class that has regular expressions for validation, also creating PaymentToSupplierRecord class

---
+++ DebtManagment/DebtManagment_BusinessLayer/PaymentToSupplierRecord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtManagment_BusinessLayer
+{
+    class PaymentToSupplierRecord
+    {
+    }
+}
