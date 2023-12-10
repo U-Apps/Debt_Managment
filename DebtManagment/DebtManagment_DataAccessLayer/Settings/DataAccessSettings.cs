dee411517d442bbb7ac76102f25a78d2b5fcd02a
Saleh_Mohammed <salhbnsmyd3@gmail.com>
2023-12-10 16:39:12 +0300

    reset the project files

--- DebtManagment/DebtManagment_DataAccessLayer/Settings/DataAccessSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace DebtManagment_DataAccessLayer
-{
-     static class clsDataAccessSettings
-    {
-        public static string ConnectionString = "Server=.;Database=Debt_Managment_DB;User Id=sa;Password=sa123456;";
-    }
-}
